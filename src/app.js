7ee7be025cab1b4c921d2f65b15730d43b6a2da7
skifozzze <skifozzze@gmail.com>
2023-04-03 20:07:08 +0300

    add timeout

---
+++ src/app.js
@@ -0,0 +1,1 @@
+import timeout from './js/01-timeout';
---
+++ src/js/01-timeout.js
@@ -0,0 +1,34 @@
+// const logMessage = () => {
+//   console.log('лог при виклику коллбек функції через 3 секунди');
+// };
+
+// console.log('до виклику setTimeout');
+
+// setTimeout(
+//   x => {
+//     console.log(x);
+//     console.log('1- всередині коллбек для setTimeout');
+//   },
+//   2000,
+//   5
+// );
+
+// setTimeout(() => {
+//   console.log('2- всередині коллбек для setTimeout');
+// }, 1000);
+
+// console.log('після виклику setTimeout');
+
+const logger = time => {
+  console.log(`лог через ${time} ms, тому що не відмінили таймаут`);
+};
+
+const timerId = setTimeout(logger, 3000, 2000);
+console.log(timerId);
+
+const shouldCancelTimer = Math.random() > 0.3;
+console.log(shouldCancelTimer);
+
+if (shouldCancelTimer) {
+  clearTimeout(timerId);
+}
